57146cf7d4ff98a83dbf0f5331a325eb38239851
Stephanie Zhu <zhulei23@gmail.com>
2020-12-29 01:36:01 -0500

    Created repo for Plot.ly challenge

---
+++ static/js/app.js
@@ -0,0 +1,15 @@
+//Use the D3 library to read in samples.json
+
+
+
+//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+
+
+//Use sample_values as the values for the bar chart.
+
+
+//Use otu_ids as the labels for the bar chart.
+
+
+//Use otu_labels as the hovertext for the chart.
